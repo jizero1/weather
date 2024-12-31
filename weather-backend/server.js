e438180821b69fba41dc36050f8bb234d666de34
jizero4101@naver.com <jizero4101@naver.com>
2024-12-31 21:04:04 +0900

    nodejs추가

---
+++ weather-backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.get('/weather', (req, res) => {
+    const weatherData = {
+        location: 'Seoul',
+        temperature: 22,
+        description: 'clear sky',
+    };
+    res.json(weatherData);
+});
+
+app.listen(port, () => {
+    console.log("server started....");
+    console.log(`server is running at http://localhost:${port}`);
+});
