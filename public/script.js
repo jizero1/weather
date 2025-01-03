1643e694e1e85bdc94cc2fe224c401d12ac763b7
jizero4101@naver.com <jizero4101@naver.com>
2025-01-04 01:03:59 +0900

    이미지 추가

--- public/script.js
+++ public/script.js
@@ -1,5 +1,4 @@
-// api키를 숨기기 위해 .gitignore파일에 apikey.js파일을 넣음. (github에 표시안됨)
-// import { key } from './apikey.js'; 
+
 
 // ---------------- 현재 날짜와 요일 표시 부분 ---------------- //
 let now = new Date();
@@ -22,28 +21,6 @@ document.querySelector('.date').innerText=`${dateAdd}`;
 
 
 // ---------------- 날씨 정보 표시 ---------------- //
-// Clear sky (맑은 하늘) sun
-// Few clouds (구름 조금) cloudSun
-// Scattered clouds (흩어져 있는 구름) cloud
-// Broken clouds (구름이 조금씩 깨어짐) cloud
-// Overcast clouds (완전한 흐림) cloud
-// Shower rain (소나기 비) rain1
-// Rain (비) rain2
-// Thunderstorm (천둥번개) blot
-// Snow (눈) snow
-// Mist (안개) smog
-// Haze (연기) smog
-
-// 상태코드
-// 맑은날씨 = 800
-// 구름조금 = 801
-// 구름 = 802 ~ 804
-// 가벼운비 = 300 ~ 321
-// 비 = 500 ~ 531
-// 천둥번개 = 200~232
-// 눈 = 600~622
-// 안개,연기,흐림 = 700 ~ 771
-// 바람 = 900
 
 // 현재 위치 기반으로 날씨 확인하기
 navigator.geolocation.getCurrentPosition(function(position) {
@@ -55,38 +32,41 @@ navigator.geolocation.getCurrentPosition(function(position) {
     .then(response => response.json())
     .then(data => {
         const city = data.name; // 도시이름
-        const temp = data.main.temp;
-        const description = data.weather[0].description; // 날씨의 상태
+        const temp = data.main.temp; // 기온
+        const weather = data.weather[0].id; // 날씨 상태코드 (ex. 800은 맑음)
+        const clouds = data.clouds.all; // 구름의 양
         const humidity = data.main.humidity; // 습도
-        
-        // 만약, description이 ~~이면, weather-icon에 ~~아이콘을 추가한다.
-        const sun = `<i class="fa-solid fa-sun"></i>`;
-        const cloudSun = `<i class="fa-solid fa-cloud-sun"></i>`;
-        const cloud = `<i class="fa-solid fa-cloud"></i>`;
-        const smog = `<i class="fa-solid fa-smog"></i>`;
-        const rain1 = `<i class="fa-solid fa-cloud-showers-heavy"></i>`; // 소나기
-        const rain2 = `<i class="fa-solid fa-cloud-rain"></i>`; // 일반적인 비
-        const bolt = `<i class="fa-solid fa-cloud-bolt"></i>`;
-        const snow = `<i class="fa-solid fa-snowflake"></i>`;
+        const windSpeed = data.wind.speed; // 바람 속도
+        const sunrise = data.sys.sunrise; // 일출 시간
+        const sunset = data.sys.sunset; // 일몰 시간
 
-        console.log(description);
+        const sunriseDate = new Date(sunrise * 1000);
+        const sunsetDate = new Date(sunset * 1000);
+
+        document.querySelector('.sunrise').insertAdjacentText('beforeend',`${sunriseDate.toLocaleTimeString()}`);
+        document.querySelector('.sunset').insertAdjacentText('beforeend',`${sunsetDate.toLocaleTimeString()}`);
+        document.querySelector('.windSpeed').insertAdjacentText('beforeend',`${windSpeed}m/s`);
+        document.querySelector('.clouds').insertAdjacentText('beforeend',`${clouds}`);
+        document.querySelector('.hum').insertAdjacentText('beforeend',`${humidity}%`);
+
         const icon = document.querySelector('.icon');
-        icon.innerHTML = '';
         const statusText = document.querySelector('.statusText');
 
-        switch(description) {
-            case "clear sky" : icon.innerHTML = sun; statusText.innerText = "맑은 하늘"; break; //맑은 하늘
-            case "few clouds" : icon.innerHTML = cloudSun; statusText.innerText = "구름 조금"; break; //구름조금
-            case "scattered clouds" : icon.innerHTML = cloud; statusText.innerText = "흐린 하늘"; break; //흩어져있는 구름
-            case "broken clouds" : icon.innerHTML = cloud; statusText.innerText = "구름 많음"; break; //구름이 깨진 구름
-            case "overcast clouds": icon.innerHTML = cloud; statusText.innerText = "완전히 흐림"; break; //완전한 흐림
-            case "shower rain" : icon.innerHTML = rain1; statusText.innerText = "소나기"; break; //소나기
-            case "rain" : icon.innerHTML = rain2; statusText.innerText = "비"; break;//비
-            case "thunderstorm" : icon.innerHTML = bolt; statusText.innerText = "천둥 번개";break; //천둥번개
-            case "snow" : case "light snow" : icon.innerHTML = snow; statusText.innerText = "눈"; break;//눈
-            case "mist": case "haze": icon.innerHTML = smog; statusText.innerText = "안개";break; //안개,연기
+        switch(weather) {
+            case 800 : icon.innerText = `sunny`; statusText.innerHTML = '맑음'; break; // 맑은 하늘
+            case 801 : icon.innerText = `partly_cloudy_day`; statusText.innerHTML = '구름 조금'; break; // 구름 조금
+            case 802 : case 803 : case 804 : icon.innerText = `cloud`; statusText.innerHTML = '구름 많음'; break; // 구름 많음
+            case 701 : case 741 : icon.innerText = `foggy`; statusText.innerHTML = '안개'; break; // 안개
+            case 711 : case 721 : icon.innerText = `mist`; statusText.innerHTML = '안개와 연기';  break; // 연기
+            case 731 : case 751 : case 761 : case 762 : case 771 : case 781 : icon.innerText = `storm`; statusText.innerHTML = '폭풍'; break; // 폭풍
+            case 600 : case 601 : case 613 : icon.innerText = `weather_snowy`; statusText.innerHTML = '적은 양의 눈'; break; // 가벼운 눈
+            case 602 : icon.innerText = `snowing_heavy`; statusText.innerHTML = '많은 양의 눈'; break; // 강한 눈
+            case 611 : case 612 : case 615 : case 616 : case 620 : case 621 : case 622 : case 511 : icon.innerText = `rainy_snow`; statusText.innerHTML = '눈과 비';break; // 눈과 비
+            case 500 : case 501 : case 520 : case 300 : case 301 : case 302 : case 311 : icon.innerText = `rainy`; statusText.innerHTML = '적은 양의 비'; break; // 가벼운 비
+            case 502 : case 503 : case 504 : case 521 : case 522 : case 531 : case 310 : case 312 : case 313 : case 314 : case 321 : icon.innerText = `rainy_heavy`;statusText.innerHTML = '많은 양의 비'; break; // 강한 비
+            case 200 : case 201 : case 202 : case 210 : case 211 : case 212 : case 221 : case 230 : case 231 : case 232 : icon.innerText = `thunderstorm`; statusText.innerHTML = '천둥번개'; break; // 천둥번개
         }
-        // document.querySelector('.status').innerText=`${description}`;
+
         document.querySelector('.temp').innerText=`${temp.toFixed(2)}°C`;
         document.querySelector('.location').innerText=`${city}`;
     })
