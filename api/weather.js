ca42d27321427b288d7ab46a942f4afd0a85f0fd
jizero4101@naver.com <jizero4101@naver.com>
2025-01-03 18:51:13 +0900

    반응형 추가

--- api/weather.js
+++ api/weather.js
@@ -53,41 +53,4 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-
-
-
-
-// // api/weather.js
-// import express from 'express';
-// import axios from 'axios';
-// import cors from 'cors';
-
-// const app = express();
-// app.use(cors());
-
-// app.get('/weather', async (req, res) => {
-//     const { lat, lon } = req.query;
-
-//     if (!lat || !lon) {
-//         return res.status(400).json({ error: 'Latitude and Longitude are required' });
-//     }
-
-//     try {
-//         const response = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
-//             params: {
-//                 lat: lat,
-//                 lon: lon,
-//                 appid: process.env.OPENWEATHER_API_KEY,
-//                 units: 'metric',
-//                 lang: 'kr',
-//             },
-//         });
-
-//         res.json(response.data);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ error: 'Failed to fetch weather data' });
-//     }
-// });
-
 export default app;  // Vercel에서는 이렇게 export 해야 합니다.
